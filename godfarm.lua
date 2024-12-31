8b25eac657bb05242ca46233c538ff728b460912
Maximusikk <78302672+Maximusikk@users.noreply.github.com>
2024-12-31 20:11:25 +0500

    Update godfarm.lua

--- godfarm.lua
+++ godfarm.lua
@@ -1,13 +1,9 @@
--- Скачивание и запуск первого скрипта в модуле
 spawn(function()
     local godsystem = loadstring(game:HttpGet("https://raw.githubusercontent.com/Maximusikk/newScript/refs/heads/main/godsystem.lua"))()
-    -- Запуск кода или функций из godsystem
 end)
 
--- Задержка, затем запуск второго скрипта
 wait(10)
 
 spawn(function()
     local subfarm = loadstring(game:HttpGet("https://raw.githubusercontent.com/Maximusikk/newScript/refs/heads/main/subfarm.lua"))()
-    -- Запуск кода или функций из subfarm
 end)
