9f6a83c795fe93b077ac9036b022633d84f7e96b
Maximusikk <78302672+Maximusikk@users.noreply.github.com>
2024-12-31 19:56:47 +0500

    Update godfarm.lua

--- godfarm.lua
+++ godfarm.lua
@@ -1,3 +1,5 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Maximusikk/newScript/refs/heads/main/subfarm.lua"))()
-wait(10)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Maximusikk/newScript/refs/heads/main/godsystem.lua"))()
+wait(10)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Maximusikk/newScript/refs/heads/main/subfarm.lua"))()
+
+
