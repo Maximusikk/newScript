d655bb364fed8557540b61f1a9a010c994ccc162
Maximusikk <78302672+Maximusikk@users.noreply.github.com>
2024-12-31 20:11:47 +0500

    Update main2.lua

--- main2.lua
+++ main2.lua
@@ -2,7 +2,7 @@ spawn(function()
     local godsystem = loadstring(game:HttpGet("https://raw.githubusercontent.com/Maximusikk/newScript/refs/heads/main/godsystem.lua"))()
 end)
 
-wait(10)
+wait(1)
 
 spawn(function()
     local subfarm = loadstring(game:HttpGet("https://raw.githubusercontent.com/Maximusikk/newScript/refs/heads/main/subfarm.lua"))()
